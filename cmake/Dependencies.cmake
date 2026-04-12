f0b259a49d59cd8f7281064af9284efe8a8a368b
Luca Mignatti <lucamignatti@icloud.com>
2026-04-12 15:37:40 -0400

    fix compile

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,7 +1,210 @@
 include(FetchContent)
 
+function(_pulsar_suppress_external_warnings_begin)
+  set(_PULSAR_SAVED_CMAKE_WARN_DEPRECATED "${CMAKE_WARN_DEPRECATED}" PARENT_SCOPE)
+  set(_PULSAR_SAVED_CMAKE_SUPPRESS_DEVELOPER_WARNINGS "${CMAKE_SUPPRESS_DEVELOPER_WARNINGS}" PARENT_SCOPE)
+  set(CMAKE_WARN_DEPRECATED FALSE PARENT_SCOPE)
+  set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 PARENT_SCOPE)
+endfunction()
+
+function(_pulsar_suppress_external_warnings_end)
+  set(CMAKE_WARN_DEPRECATED "${_PULSAR_SAVED_CMAKE_WARN_DEPRECATED}" PARENT_SCOPE)
+  set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS "${_PULSAR_SAVED_CMAKE_SUPPRESS_DEVELOPER_WARNINGS}" PARENT_SCOPE)
+endfunction()
+
+function(_pulsar_append_prefix_if_exists prefix)
+  if(prefix AND EXISTS "${prefix}")
+    list(APPEND CMAKE_PREFIX_PATH "${prefix}")
+    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" PARENT_SCOPE)
+  endif()
+endfunction()
+
+function(_pulsar_rocm_prefixes out_var)
+  set(_rocm_prefixes "")
+
+  if(DEFINED ENV{ROCM_PATH} AND NOT "$ENV{ROCM_PATH}" STREQUAL "")
+    list(APPEND _rocm_prefixes "$ENV{ROCM_PATH}")
+  endif()
+  if(DEFINED ENV{HIP_PATH} AND NOT "$ENV{HIP_PATH}" STREQUAL "")
+    list(APPEND _rocm_prefixes "$ENV{HIP_PATH}")
+  endif()
+
+  list(APPEND _rocm_prefixes
+    /opt/rocm
+    /usr
+    /usr/local
+  )
+
+  set(${out_var} "${_rocm_prefixes}" PARENT_SCOPE)
+endfunction()
+
+function(_pulsar_add_rocm_prefixes)
+  _pulsar_rocm_prefixes(_rocm_prefixes)
+
+  foreach(_prefix IN LISTS _rocm_prefixes)
+    _pulsar_append_prefix_if_exists("${_prefix}")
+    _pulsar_append_prefix_if_exists("${_prefix}/hip")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib/cmake")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib64/cmake")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib/cmake/hip")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib64/cmake/hip")
+    _pulsar_append_prefix_if_exists("${_prefix}/hip/lib/cmake")
+    _pulsar_append_prefix_if_exists("${_prefix}/hip/lib64/cmake")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib/cmake/hiprtc")
+    _pulsar_append_prefix_if_exists("${_prefix}/lib64/cmake/hiprtc")
+    _pulsar_append_prefix_if_exists("${_prefix}/hip/lib/cmake/hiprtc")
+    _pulsar_append_prefix_if_exists("${_prefix}/hip/lib64/cmake/hiprtc")
+  endforeach()
+endfunction()
+
+function(_pulsar_detect_torch_install_prefix out_var)
+  set(_torch_prefix "")
+
+  if(DEFINED Torch_DIR AND EXISTS "${Torch_DIR}")
+    get_filename_component(_torch_prefix "${Torch_DIR}/../../../" ABSOLUTE)
+  else()
+    foreach(_prefix IN LISTS CMAKE_PREFIX_PATH)
+      if(EXISTS "${_prefix}/Torch/TorchConfig.cmake")
+        get_filename_component(_torch_prefix "${_prefix}/../.." ABSOLUTE)
+        break()
+      endif()
+      if(EXISTS "${_prefix}/share/cmake/Torch/TorchConfig.cmake")
+        get_filename_component(_torch_prefix "${_prefix}" ABSOLUTE)
+        break()
+      endif()
+    endforeach()
+  endif()
+
+  set(${out_var} "${_torch_prefix}" PARENT_SCOPE)
+endfunction()
+
+function(_pulsar_prepare_torch_optional_libs)
+  _pulsar_detect_torch_install_prefix(_torch_prefix)
+  if(NOT _torch_prefix)
+    return()
+  endif()
+
+  if(NOT DEFINED kineto_LIBRARY OR NOT kineto_LIBRARY)
+    foreach(_fallback IN ITEMS
+      "${_torch_prefix}/lib/libtorch.so"
+      "${_torch_prefix}/lib/libtorch_cpu.so"
+      "${_torch_prefix}/lib/libc10.so"
+      "${_torch_prefix}/lib/libtorch.dylib"
+      "${_torch_prefix}/lib/libtorch_cpu.dylib"
+      "${_torch_prefix}/lib/libc10.dylib"
+    )
+      if(EXISTS "${_fallback}")
+        set(kineto_LIBRARY "${_fallback}" CACHE FILEPATH "Fallback path for optional kineto library." FORCE)
+        break()
+      endif()
+    endforeach()
+  endif()
+endfunction()
+
+function(_pulsar_alias_imported_target alias)
+  if(TARGET "${alias}")
+    return()
+  endif()
+
+  foreach(candidate IN LISTS ARGN)
+    if(TARGET "${candidate}")
+      add_library("${alias}" INTERFACE IMPORTED)
+      target_link_libraries("${alias}" INTERFACE "${candidate}")
+      return()
+    endif()
+  endforeach()
+endfunction()
+
+function(_pulsar_find_rocm_package package_name)
+  _pulsar_add_rocm_prefixes()
+  _pulsar_suppress_external_warnings_begin()
+  find_package(${package_name} QUIET CONFIG)
+  _pulsar_suppress_external_warnings_end()
+  if(${package_name}_FOUND)
+    message(STATUS "Found ${package_name}")
+  endif()
+  set(${package_name}_FOUND "${${package_name}_FOUND}" PARENT_SCOPE)
+endfunction()
+
+function(pulsar_find_rocm_hip)
+  _pulsar_add_rocm_prefixes()
+
+  _pulsar_suppress_external_warnings_begin()
+  find_package(hip QUIET CONFIG)
+  _pulsar_suppress_external_warnings_end()
+
+  if(hip_FOUND)
+    message(STATUS "Found HIP: ${hip_DIR}")
+    if((NOT DEFINED ENV{ROCM_PATH} OR "$ENV{ROCM_PATH}" STREQUAL "") AND hip_DIR)
+      get_filename_component(_hip_cmake_dir "${hip_DIR}" DIRECTORY)
+      get_filename_component(_hip_lib_dir "${_hip_cmake_dir}" DIRECTORY)
+      get_filename_component(_hip_prefix "${_hip_lib_dir}" DIRECTORY)
+      if(EXISTS "${_hip_prefix}")
+        set(ENV{ROCM_PATH} "${_hip_prefix}")
+      endif()
+    endif()
+  endif()
+
+  set(hip_FOUND ${hip_FOUND} PARENT_SCOPE)
+  set(hip_DIR ${hip_DIR} PARENT_SCOPE)
+endfunction()
+
+function(pulsar_find_rocm_hiprtc)
+  _pulsar_find_rocm_package(hiprtc)
+
+  if(TARGET hiprtc AND NOT TARGET hiprtc::hiprtc)
+    add_library(hiprtc::hiprtc INTERFACE IMPORTED)
+    target_link_libraries(hiprtc::hiprtc INTERFACE hiprtc)
+  endif()
+
+  if(hiprtc_FOUND OR TARGET hiprtc::hiprtc)
+    message(STATUS "Found HIPRTC")
+  endif()
+
+  set(hiprtc_FOUND ${hiprtc_FOUND} PARENT_SCOPE)
+endfunction()
+
+function(pulsar_find_rocm_libraries)
+  _pulsar_find_rocm_package(hipblas)
+  _pulsar_find_rocm_package(hipblaslt)
+  _pulsar_find_rocm_package(hipfft)
+  _pulsar_find_rocm_package(hiprand)
+  _pulsar_find_rocm_package(hipsparse)
+  _pulsar_find_rocm_package(hipsparselt)
+  _pulsar_find_rocm_package(hipsolver)
+  _pulsar_find_rocm_package(hiptensor)
+  _pulsar_find_rocm_package(rocblas)
+  _pulsar_find_rocm_package(rocfft)
+  _pulsar_find_rocm_package(rocrand)
+  _pulsar_find_rocm_package(rocsolver)
+  _pulsar_find_rocm_package(MIOpen)
+  _pulsar_find_rocm_package(rccl)
+
+  _pulsar_alias_imported_target("roc::hipblas" "hipblas::hipblas" "hipblas")
+  _pulsar_alias_imported_target("roc::hipblaslt" "hipblaslt::hipblaslt" "hipblaslt")
+  _pulsar_alias_imported_target("roc::hipfft" "hipfft::hipfft" "hipfft")
+  _pulsar_alias_imported_target("hip::hiprand" "hiprand::hiprand" "hiprand")
+  _pulsar_alias_imported_target("roc::hiprand" "hiprand::hiprand" "hiprand")
+  _pulsar_alias_imported_target("roc::hipsparse" "hipsparse::hipsparse" "hipsparse")
+  _pulsar_alias_imported_target("roc::hipsparselt" "hipsparselt::hipsparselt" "hipsparselt")
+  _pulsar_alias_imported_target("roc::hipsolver" "hipsolver::hipsolver" "hipsolver")
+  _pulsar_alias_imported_target("roc::hiptensor" "hiptensor::hiptensor" "hiptensor")
+  _pulsar_alias_imported_target("roc::rocblas" "rocblas::rocblas" "rocblas")
+  _pulsar_alias_imported_target("roc::rocfft" "rocfft::rocfft" "rocfft")
+  _pulsar_alias_imported_target("roc::rocrand" "rocrand::rocrand" "rocrand")
+  _pulsar_alias_imported_target("roc::rocsolver" "rocsolver::rocsolver" "rocsolver")
+  _pulsar_alias_imported_target("MIOpen" "MIOpen::MIOpen")
+  _pulsar_alias_imported_target("rccl::rccl" "rccl")
+endfunction()
+
 function(pulsar_find_torch)
+  pulsar_find_rocm_hip()
+  pulsar_find_rocm_hiprtc()
+  pulsar_find_rocm_libraries()
+  _pulsar_prepare_torch_optional_libs()
+  _pulsar_suppress_external_warnings_begin()
   find_package(Torch QUIET)
+  _pulsar_suppress_external_warnings_end()
 
   if(Torch_FOUND)
     message(STATUS "Found Torch: ${TORCH_INSTALL_PREFIX}")
@@ -14,8 +217,17 @@ function(pulsar_find_torch)
 endfunction()
 
 function(pulsar_find_python_bindings)
+  set(PYBIND11_FINDPYTHON ON)
   find_package(Python3 COMPONENTS Interpreter Development.Module QUIET)
-  find_package(pybind11 QUIET)
+
+  if(Python3_FOUND)
+    _pulsar_suppress_external_warnings_begin()
+    find_package(pybind11 QUIET)
+    _pulsar_suppress_external_warnings_end()
+  else()
+    message(STATUS "Python3 Development.Module not found; skipping pybind11 discovery.")
+    set(pybind11_FOUND FALSE)
+  endif()
 
   if(NOT pybind11_FOUND AND PULSAR_FETCH_PYBIND11)
     FetchContent_Declare(
@@ -23,7 +235,9 @@ function(pulsar_find_python_bindings)
       GIT_REPOSITORY https://github.com/pybind/pybind11.git
       GIT_TAG v2.13.6
     )
-    FetchContent_MakeAvailable(pybind11)
+    if(Python3_FOUND)
+      FetchContent_MakeAvailable(pybind11)
+    endif()
   endif()
 
   set(Python3_FOUND ${Python3_FOUND} PARENT_SCOPE)
